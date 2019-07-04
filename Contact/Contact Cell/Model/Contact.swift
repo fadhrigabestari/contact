1aa594ee715079642873a3ad603bb480f8449ed2
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-07-04 14:43:16 +0700

    create initial model for contact

---
+++ Contact/Contact Cell/Model/Contact.swift
@@ -0,0 +1,16 @@
+//
+//  Contact.swift
+//  Contact
+//
+//  Created by PT. GOJEK INDONESIA on 04/07/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Contact {
+    var image: UIImage
+    var name: String
+    var isFavourite: Bool
+}
