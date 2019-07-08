33d7006d4fd67eece03842d5b4e1e89e199ae5a1
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-07-08 11:54:32 +0700

    created contact detail entity

---
+++ Contact/Contact Detail/Entity/ContactDetail.swift
@@ -0,0 +1,33 @@
+//
+//  ContactDetail.swift
+//  Contact
+//
+//  Created by PT. GOJEK INDONESIA on 08/07/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import Foundation
+
+struct ContactDetail: Codable {
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var email: String
+    var phoneNumber: String
+    var profilePic: String
+    var isFavorite: Bool
+    var createdAt: Date
+    var updatedAt: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email
+        case phoneNumber = "phone_number"
+        case profilePic = "profile_pic"
+        case isFavorite = "favorite"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
---
+++ Contact/Contact Detail/Protocol/ContactDetailProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  ContactDetailProtocol.swift
+//  Contact
+//
+//  Created by PT. GOJEK INDONESIA on 08/07/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol IContactDetailView {
+    func showContactDetail(contactDetail: ContactDetail)
+}
+
+protocol IContactDetailPresenter {
+    
+}
+
+protocol IContactDetailInteractor {
+    
+}
+
+protocol IContactDetailWireframe {
+    
+}
