04b00c52b53b298b4f6652b03270bd22f4a2cc50
muhammad.bestari <muhammad.bestari@go-jek.com>
2019-07-08 11:37:31 +0700

    initial viper directory structure created

---
+++ Contact/Contact Detail/View/ContactDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactDetailViewController.swift
+//  Contact
+//
+//  Created by PT. GOJEK INDONESIA on 08/07/19.
+//  Copyright © 2019 PT. GOJEK INDONESIA. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
